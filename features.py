5666a4bb65a1cc772677f767e70463493dd8fc25
Yang Yuan <yangyuan@users.noreply.github.com>
2018-01-29 19:26:32 -0500

    use char-grams as features and looks promising

--- features.py
+++ features.py
@@ -1,108 +1,24 @@
-import re
-
-
-class RawFeatures:
-
-    @staticmethod
-    def digits_only(token):
-        if re.match('[0-9]+$', token):
-            return True
-        return False
-
-    @staticmethod
-    def digits_and_dot_1(token):
-        if re.match('\.[0-9]+$', token):
-            return True
-        return False
-
-    @staticmethod
-    def digits_and_dot_2(token):
-        if re.match('[0-9]+\.$', token):
-            return True
-        return False
-
-    @staticmethod
-    def digits_and_dot_3(token):
-        if re.match('[0-9]+\.[0-9]+$', token):
-            return True
-        return False
-
-    @staticmethod
-    def year_like_1(token):
-        if re.match('[1-9][0-9]{0,3}$', token):
-            return True
-        return False
-
-    @staticmethod
-    def year_like_2(token):
-        if re.match('[1-9][0-9]{1,3}$', token):
-            return True
-        return False
-
-    @staticmethod
-    def year_like_3(token):
-        if re.match('[1-9][0-9]{2,3}$', token):
-            return True
-        return False
-
-    @staticmethod
-    def year_like_4(token):
-        if re.match('[1-2][0-9]{3}$', token):
-            return True
-        return False
-
-    @staticmethod
-    def length_less_1(token):
-        if len(token) < 2:
-            return True
-        return False
-
-    @staticmethod
-    def length_less_2(token):
-        if len(token) < 4:
-            return True
-        return False
-
-    @staticmethod
-    def length_less_3(token):
-        if len(token) < 8:
-            return True
-        return False
-
-    @staticmethod
-    def length_less_4(token):
-        if len(token) < 16:
-            return True
-        return False
-
-    @staticmethod
-    def start_with_upper_1(token):
-        if re.match('[A-Z][a-z0-9]*$', token):
-            return True
-        return False
-
-    @staticmethod
-    def start_with_upper_2(token):
-        if re.match('[A-Z][a-z]*$', token):
-            return True
-        return False
-
-    @staticmethod
-    def all_upper(token):
-        if re.match('[A-Z]*$', token):
-            return True
-        return False
-
-    @staticmethod
-    def all_upper_2(token):
-        if re.match('[0-9]*[A-Z]*$', token):
-            return True
-        return False
-
-
 class CharFeatures:
+    chars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5',
+             '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
+             '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
 
-    chars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
+
+    @staticmethod
+    def _get_letter_grams(_len):
+        if _len == 1:
+            return CharFeatures.letters.copy()
+        else:
+            tmp = CharFeatures._get_letter_grams(_len - 1)
+            ret = []
+            for char in CharFeatures.letters:
+                for gram in tmp:
+                    ret.append(char + gram)
+            return ret
 
     @staticmethod
     def _get_grams(_len):
@@ -138,17 +54,17 @@ class CharFeatures:
             tmp = 'lambda x: x.endswith(%s)' % gram
             ret.append(eval(tmp))
 
+        for gram in CharFeatures._get_letter_grams(3):
+            gram = _char_to_py_string(gram)
+            tmp = 'lambda x: True if x.lower().find(%s) != -1 else False' % gram
+            ret.append(eval(tmp))
+
         return ret
 
 
 class Features:
     def __init__(self):
-        self._features = [getattr(RawFeatures, func) for func in dir(RawFeatures)
-                          if callable(getattr(RawFeatures, func)) and not func.startswith('__')]
-
-        # self._features = []
-
-        self._features.extend(CharFeatures.get_features())
+        self._features = CharFeatures.get_features()
         self._size = len(self._features)
 
     def get_size(self):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from features import Features
 from embedding import Glove
 from utils import GloveDataSet, GeneralDataSet
+import utils
 import tensorflow as tf
 import numpy as np
 
@@ -9,68 +10,50 @@ glove = Glove()
 # glove.parse('data/glove.840B.300d.txt')
 # glove.dump('data/glove.840B.300d')
 
-chars = set()
-glove.load('data/glove.840B.300d')
-
-
-data = GloveDataSet(glove, Features())
-data.dump('data/dataset')
+# glove.load('data/glove.840B.300d')
+# data = GloveDataSet(glove, Features())
+# data.dump('data/dataset')
 
 data = GeneralDataSet()
 data.load('data/dataset')
 
-_x, _y = data.all_data()
+_x, _y = data.all_train_data()
+_x_t, _y_t = data.test_data()
 
-x = tf.placeholder(tf.float32, (_x.shape[1], None))
-y = tf.placeholder(tf.float32, (_y.shape[1], None))
-
-w1 = tf.get_variable('w1', (1000, _x.shape[1]), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
-b1 = tf.get_variable('b1', (1000, 1), initializer=tf.random_normal_initializer())
-w2 = tf.get_variable('w2', (800, 1000), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
-b2 = tf.get_variable('b2', (800, 1), initializer=tf.random_normal_initializer())
-w3 = tf.get_variable('w3', (600, 800), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
-b3 = tf.get_variable('b3', (600, 1), initializer=tf.random_normal_initializer())
-w4 = tf.get_variable('w4', (400, 600), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
-b4 = tf.get_variable('b4', (400, 1), initializer=tf.random_normal_initializer())
-w5 = tf.get_variable('w5', (_y.shape[1], 400), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.00001))
-b5 = tf.get_variable('b5', (_y.shape[1], 1), initializer=tf.random_normal_initializer())
-
-w0 = tf.get_variable('w0', (_y.shape[1], 1000), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.000001))
-b0 = tf.get_variable('b0', (_y.shape[1], 1), initializer=tf.random_normal_initializer())
+x = tf.placeholder(tf.float32, (None, _x.shape[1]))
+y = tf.placeholder(tf.float32, (None, _y.shape[1]))
 
 
-z1 = tf.add(tf.matmul(w1, x), b1)
-a1 = tf.nn.tanh(z1)
+layer_1 = tf.contrib.layers.fully_connected(
+    inputs=x,
+    num_outputs=1200,
+    activation_fn=tf.nn.tanh)
 
-z0 = tf.add(tf.matmul(w0, z1), b0)
-a5 = tf.nn.tanh(z0)
+layer_2 = tf.contrib.layers.fully_connected(
+    inputs=layer_1,
+    num_outputs=600,
+    activation_fn=tf.nn.tanh)
 
-'''
-z2 = tf.add(tf.matmul(w2, z1), b2)
-a2 = tf.nn.tanh(z2)
+_out = tf.contrib.layers.fully_connected(
+    inputs=layer_2,
+    num_outputs=_y.shape[1],
+    activation_fn=tf.nn.tanh)
 
-z3 = tf.add(tf.matmul(w3, z2), b3)
-a3 = tf.nn.tanh(z3)
+cost = tf.reduce_mean(tf.pow(_out - y, 2))
 
-z4 = tf.add(tf.matmul(w4, z3), b4)
-a4 = tf.nn.tanh(z4)
-
-z5 = tf.add(tf.matmul(w5, z4), b5)
-a5 = tf.nn.tanh(z5)
-
-'''
-
-cost = tf.reduce_mean(tf.pow(a5 - y, 2))
-
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 init_op = tf.global_variables_initializer()
+
 
 with tf.Session() as sess:
     sess.run(init_op)
 
     for i in range(100):
-        _, c = sess.run([optimizer, cost], feed_dict={x: np.transpose(_x), y: np.transpose(_y)})
-        print(c)
+        for batch_x, batch_y in utils.batches(_x, _y, 1000):
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+            print(c)
+        c = sess.run(cost, feed_dict={x: _x_t, y: _y_t})
+        print('test: ', c)
 
 
